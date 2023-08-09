7ec4e5615208102187ac74620e7e481b1302c56c
Daelum Marx <33014828+daelum@users.noreply.github.com>
2023-08-09 12:15:20 +1000

    update
    added array of objects containing each characters name and img. created a random integer function that correlates to an index position in the array them updates the html with random character img and name

--- script.js
+++ script.js
@@ -3,3 +3,99 @@ console.log('Guess Who?')
 function toggleEliminated(element) {
     element.classList.toggle('eliminated')
 }
+
+let cards = [
+    {
+        name: 'Sakura',
+        image: 'images/characters_01.jpg'
+    },
+    {
+        name: 'Kaede',
+        image: 'images/characters_02.jpg'
+    },
+    {
+        name: 'Aya',
+        image: 'images/characters_03.jpg'
+    },
+    {
+        name: 'Yuuki',
+        image: 'images/characters_04.jpg'
+    },
+    {
+        name: 'Mikoto',
+        image: 'images/characters_05.jpg'
+    },
+    {
+        name: 'Ren',
+        image: 'images/characters_06.jpg'
+    },
+    {
+        name: 'Ryo',
+        image: 'images/characters_07.jpg'
+    },
+    {
+        name: 'Yori',
+        image: 'images/characters_08.jpg'
+    },
+    {
+        name: 'Hana',
+        image: 'images/characters_09.jpg'
+    },
+    {
+        name: 'Tatsuya',
+        image: 'images/characters_10.jpg'
+    },
+    {
+        name: 'Haro',
+        image: 'images/characters_11.jpg'
+    },
+    {
+        name: 'Mai',
+        image: 'images/characters_12.jpg'
+    },
+    {
+        name: 'Emiko',
+        image: 'images/characters_13.jpg'
+    },
+    {
+        name: 'Ayumi',
+        image: 'images/characters_14.jpg'
+    },
+    {
+        name: 'Hotaru',
+        image: 'images/characters_15.jpg'
+    },
+    {
+        name: 'Haru',
+        image: 'images/characters_16.jpg'
+    },
+    {
+        name: 'Kazuya',
+        image: 'images/characters_17.jpg'
+    },
+    {
+        name: 'Akira',
+        image: 'images/characters_18.jpg'
+    },
+    {
+        name: 'Saki',
+        image: 'images/characters_19.jpg'
+    },
+    {
+        name: 'Kazuo',
+        image: 'images/characters_20.jpg'
+    },
+]
+
+//random number between 0 and 19
+function getRandomCard() {
+    let randomCard = Math.floor(Math.random() * 19)
+    return randomCard
+}
+console.log(getRandomCard())
+
+let randomCardIndex = getRandomCard()
+let selectedCard = cards[randomCardIndex]
+
+document.querySelector('.mystery-card span').innerHTML = selectedCard.name
+document.querySelector('.mystery-card img').src = selectedCard.image
