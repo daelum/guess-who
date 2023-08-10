8c8c51e974541c49d4657111c471434400a808d7
Daelum Marx <33014828+daelum@users.noreply.github.com>
2023-08-10 11:39:33 +1000

    update
    removed card divs in html and created a loop function that injects the html replacing the image and name.

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 console.log('Guess Who?')
-
+//belox is the tooggle Eliminated function which allows the cards to turn on/off 20% opactity when clicked
 function toggleEliminated(element) {
     element.classList.toggle('eliminated')
 }
-
+// below is the characters array
 let cards = [
     {
         name: 'Sakura',
@@ -89,13 +89,35 @@ let cards = [
 
 //random number between 0 and 19
 function getRandomCard() {
-    let randomCard = Math.floor(Math.random() * 19)
+    let randomCard = Math.floor(Math.random() * 19)//Math.floor seems to turn the Math.random into a whole integer
     return randomCard
 }
 console.log(getRandomCard())
 
-let randomCardIndex = getRandomCard()
-let selectedCard = cards[randomCardIndex]
+let randomCardIndex = getRandomCard()//the random number generated correlates to a index postion which is placed in this var
+let selectedCard = cards[randomCardIndex]// calling the above variable(random index postion) on the cards array and declaring it as selectedCard
 
-document.querySelector('.mystery-card span').innerHTML = selectedCard.name
-document.querySelector('.mystery-card img').src = selectedCard.image
+document.querySelector('.mystery-card span').innerHTML = selectedCard.name//pulling up the msytery-card div and replacing its inner HTML with the selectedCards name
+document.querySelector('.mystery-card img').src = selectedCard.image//accessing the 'img' tags source and replacing the card image with selected card 
+
+//function to inject data(characters) directly into the html from js
+function populateCards() {
+        let cardsBox = document.querySelector('.cards')// targeted .cards html div and assigned them to a variable
+        let injection = '' // created a variable that holds an empty string value that will be used to hold that for loop string injection
+
+        for (let card of cards){
+            injection += `
+            <div class="card" onclick="toggleEliminated(this)">
+                <img src="${card.image}" />
+                <span>${card.name}</span>
+            </div>
+            `
+        }
+        cardsBox.innerHTML = injection
+}
+/* the above funcitons for loop is a for..of loop which let me declare a variable card which is of the cards
+array it iterates through them it, takes the current value of the variable (which is nill in this case),
+it adds or 'concantenates' a new value to the current value, and it assigns the results back to the variable. 
+I used Template literals(`` aka backticks) to embed the html into the injection variable
+and a template literal placeholder `${..}` inside the html that holds the location of the injected source */
+populateCards(cards)
